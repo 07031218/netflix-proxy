da40478071a3b6d7ba48cee2acb2e3796c5d7786
07031218 <minshunqiang@gmail.com>
2021-09-18 12:52:58 +0800

    Update admin-reset.sh

--- auth/admin-reset.sh
+++ auth/admin-reset.sh
@@ -4,10 +4,10 @@
 CWD=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 SQLITE_DB=${CWD}/db/auth.db
 
-read -sp "Please enter a new admin password: " PASSWORD && printf "\n"
-
-    pushd ${CWD} && \
-      export HASH=`${CWD}/pbkdf2_sha256_hash.py ${PASSWORD} | awk '{print $2}'` && \
+read -sp "请输入欲设置的新密码: " PASSWORD && printf "\n"
+echo -e "----------开始修改密码-----------"
+    #pushd ${CWD} && \
+     HASH=$(python3 ${CWD}/pbkdf2_sha256_hash.py ${PASSWORD} | awk '{print $2}') && \
       sqlite3 ${SQLITE_DB} "UPDATE USERS SET password='${HASH}' WHERE id=1;" && \
-      popd && \
-      printf "rc=$?\n"
+    #  popd && \
+      printf "Congratulations，密码修改完成。新密码为：${PASSWORD}\n"
